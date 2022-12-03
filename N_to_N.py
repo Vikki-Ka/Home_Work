adc3657fb18863f25dd028eb9577a45bd6429471
Vikki <vikki_ka@mail.ru>
2022-12-03 22:27:39 +0300

    в коде добавили проверку на положительное число

--- N_to_N.py
+++ N_to_N.py
@@ -2,7 +2,9 @@ n=int (input("Ввидите положительное число "))
 a=n
 n=n*(-1)
 
-while n<=a:
-    print(n)
-    n+=1
-
+if a>0 :
+    while n<=a:
+        print(n)
+        n+=1
+else:
+    print("Ввидите положительное число")
