e17401095ef8cd1493d1293ca8865636d4514588
Vikki <vikki_ka@mail.ru>
2022-12-03 23:15:30 +0300

    Format document

--- N_to_N.py
+++ N_to_N.py
@@ -1,9 +1,9 @@
-a=int (input("Ввидите число "))
-n=a*(-1)
+a = int(input("Ввидите число "))
+n = a * (-1)
 
-if a>0 and a<10 :
-    while n<=a:
+if a > 0 and a < 10:
+    while n <= a:
         print(n)
-        n+=1      
+        n += 1
 else:
     print("Ввидите число больше 0 и меньше 10")
