699d08c02799441b7d39c8d241ae851b27762d28
Vikki <vikki_ka@mail.ru>
2022-12-03 22:34:29 +0300

    В код добавили проверку для числа меньше 10

--- N_to_N.py
+++ N_to_N.py
@@ -1,10 +1,13 @@
-n=int (input("Ввидите положительное число "))
+n=int (input("Ввидите число "))
 a=n
 n=n*(-1)
 
 if a>0 :
-    while n<=a:
-        print(n)
-        n+=1
+    if a<10:
+        while n<=a:
+            print(n)
+            n+=1
+    else:
+        print ("Ввидите число меньше 10")        
 else:
     print("Ввидите положительное число")
